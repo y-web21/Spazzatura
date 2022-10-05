ad40f5c41da44ccb61fc863da1785624048cefd8
y-web21 <y.web.2019@gmail.com>
2022-10-06 00:38:48 +0900

    update jstest.html(navigation topview)

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,12 +1,20 @@
 // global variables
 scrollbarWidth = 0;
-gotoTopHeight = 0
+gotoTopTagTop = 0;
+
+var y = window.pageYOffset ;
+console.log(y);
+
+// mainVisual IDから取得するとメンテが楽だけ処理負荷的にはこれでも
+const TagVisubleVolume = 480;
+const NavHiddenVolume = 2200;
+
 window.onload = function() {
-    gotoTopHeight = parseInt(getCssValue(document.querySelector('.goto_top_tag'), 'height'), 10);
+    gotoTopTagTop = parseInt(getCssValue(document.querySelector('.goto_top_tag'), 'height'), 10);
     // console.log(document.querySelector('.goto_top_tag'));
     addClassActivePageLink();
-    document.getElementsByTagName('body')[0].style.visibility = "visible";
-    console.log(document.getElementsByTagName('body'));
+    // document.getElementsByClassName('hidOnload')[0].style.visibility = "visible";
+    // document.getElementsByTagName('body')[0].style.visibility = "visible";
     // target[i].classList.add('nav_active_page');
 
 };
@@ -15,15 +23,15 @@ window.onload = function() {
 $(document).ready(function() {
     // 下にスクロールした状態で、リロードするとナビ位置がずれるため、初回にも暫定的に走らせる
     // ちらつくため、要デバッグ
-    let gnavHeight = parseInt($('.sticky_menu').css('height'), 10);
+    let mainVisualHeight = parseInt($('#mainVisual').css('height'), 10);
     let scrollVolume = $(this).scrollTop();
-    stickyNav(scrollVolume, gnavHeight);
+    stickyNav(scrollVolume, mainVisualHeight);
 });
 $(window).on('load', function() {
     // 取得する値が安定しないため、CSSの固定値決め打ちで対応
     // scrollbarWidth = window.innerWidth - document.body.clientWidth;
     //  $(".goto_top_tag").css({
-    //      "left": document.body.clientWidth - gotoTopHeight
+    //      "left": document.body.clientWidth - gotoTopTagTop
     //  })
 });
 
@@ -41,9 +49,9 @@ $(window).on('load', function() {
 // Scroll event
 var scrollTimer = 0;
 $(window).scroll(function() {
-    let gnavHeight = parseInt($('.sticky_menu').css('height'), 10);
+    let mainVisualHeight = parseInt($('#mainVisual').css('height'), 10);
     let scrollVolume = $(this).scrollTop();
-    stickyNav(scrollVolume, gnavHeight);
+    stickyNav(scrollVolume, mainVisualHeight);
 
     //処理負荷軽減
     clearTimeout(scrollTimer);
@@ -52,8 +60,8 @@ $(window).scroll(function() {
         // $('#imo').text($(this).scrollTop());
         // gotoTop(scrollVolume, 600);
 
-        chgClassWhenScrolling(scrollVolume, 700, 'goto_top_tag', 'sidetab-fadein', 'sidetab-fadeout');
-        // chgClassWhenScrolling(scrollVolume, 700, 'sticky_menu', 'z-index9999');
+        chgClassWhenScrolling(scrollVolume, TagVisubleVolume, 'goto_top_tag', 'sidetab-fadein', 'sidetab-fadeout');
+        // chgClassWhenScrolling(scrollVolume, TagVisubleVolume, 'sticky_menu', 'z-index9999');
         console.log(scrollTimer + 'ms');
         console.log(scrollVolume);
     }, 250);
@@ -62,24 +70,39 @@ $(window).scroll(function() {
 
 // jquery functions
 function stickyNav(scrollVolume, gnavY) {
-    if (scrollVolume > gnavY) {
+    if (gnavY < scrollVolume) {
         $(".stickyNav").css({
+            "position": "fixed",
             "top": 0 + "px",
             "z-index": "9999",
         });
     } else {
         $(".stickyNav").css({
+            "position": "static",
             "top": gnavY - scrollVolume + "px",
             "z-index": "auto",
         });
     }
+
+    // 表示を消す必要がある場合
+    if(NavHiddenVolume < scrollVolume) {
+        $(".stickyNav").css({
+            "position": "static",
+            "visibility": "hidden",
+        });
+    } else {
+        $(".stickyNav").css({
+            "visibility": "visible",
+        });
+    }
+
 }
 
 function gotoTop(scrollVolume, threshold) {
     if (scrollVolume > threshold) {
         $(".goto_top_tag").css({
             "display": "block",
-            "left": document.body.clientWidth - gotoTopHeight
+            "left": document.body.clientWidth - gotoTopTagTop
         });
     } else {
         $(".goto_top_tag").css({
@@ -162,9 +185,20 @@ function addClassActivePageLink(){
     for (let i=0;i < target.length; i++){
         if (target[i].attributes.href.value === currentPage){
             target[i].classList.add('nav_active_page');
+            return
         }
         // href属性が存在するかチェック
         // for (let j=0;j < target[i].attributes.length;j++){
         // }
     }
 }
+
+// ぱららさんぷる
+// $(function() {
+// $(window).scroll(function(){
+// var y = $(this).scrollTop();
+// $('#bg01').css('background-position', '0 ' + parseInt( -y / 5 ) + 'px');
+// $('#bg02').css('background-position', '0 ' + parseInt( -y / 50 ) + 'px');
+// $('#bg03').css('background-position', '0 ' + parseInt( -y / 200 ) + 'px');
+// });
+// });
