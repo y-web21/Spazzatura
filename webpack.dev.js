5d506fb120de5f5ff86481da9c4380e72f69342f
y-web21 <51986098+y-web21@users.noreply.github.com>
2022-11-26 04:22:49 +0000

    [#1] webpack の記述ミス修正

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
