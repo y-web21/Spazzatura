46cdade982c4b4e29c98d1475c93fe17e8a14f6e
y-web21 <51986098+y-web21@users.noreply.github.com>
2022-11-27 10:29:15 +0000

    [#23] webpack dev server, bs 設定

---
+++ bs-config.js
@@ -0,0 +1,24 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "ui": {
+        "port": 3001
+    },
+    "port": 3000,
+    "files": ["dist/*.html", "dist/**/*.css","dist/**/*.js"],
+    "server": ['dist'],
+    "proxy": false,
+    "startPath": '/index.html'
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,20 +2,25 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+/** @type {import('webpack').Configuration} */
 module.exports = merge(common, {
   mode: 'development',
-  module: {
-    options: {
-      sourceMap: true,
-    },
-  },
-  watch: true,
+  watch: true,  // dev sever には不要な設定
   watchOptions:{
     ignored: /node_modules/
   },
-  devtool: "hidden-source-map",
-  module: {
-    rules: [
-    ],
+  devtool: ['source-map', "inline-source-map", "hidden-source-map"][0],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+    open: true,
+    port: 8080,
+    // host: '0.0.0.0', // docker を使うならデフォルトルートを設定する
+    watchFiles: ['src/**/*', 'dist/**/*html'],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
   },
 });
